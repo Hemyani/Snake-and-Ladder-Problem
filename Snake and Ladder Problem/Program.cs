bf23d3cdccd2d23386accc1ccf628578623eec9b
Hemyani <hdg2512@gmail.com>
2022-11-24 15:12:52 +0530

    Add project files.

---
+++ Snake and Ladder Problem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Snake_and_Ladder_Problem;
+
+namespace Snake_And_Ladder_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1.Position();
+            Console.ReadLine();
+
+            //UC2.rollsthedie();
+            //Console.ReadLine();
+
+            //UC3.CheckOption();
+            //Console.ReadLine();
+
+            //UC4.Repeat();
+            //Console.ReadLine();
+
+            //UC5.WinPosition();
+            //Console.ReadLine();
+
+            //UC6.Report();
+            //Console.ReadLine();
+
+            //UC7.PlayerWon();
+            //Console.ReadLine();
+        }
+    }
+}
